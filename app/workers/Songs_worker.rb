b8bb7a4c9a41f06d1642da326ca598d2ab675817
Kirby Campbell <jkirbycampbell@gmail.com>
2018-10-22 22:41:13 +0000

    automatically backed up by learn

--- app/workers/Songs_worker.rb
+++ app/workers/Songs_worker.rb
@@ -3,7 +3,7 @@ class SongsWorker
   require 'csv'
 
   def perform(songs_file)
-    #CSV.foreach(leads_file, headers: true) do |lead|
+    CSV.foreach(songs_file, headers: true) do |song|
     #Customer.create(email: lead[0], first_name: lead[1], last_name: lead[2])
   end
 end
