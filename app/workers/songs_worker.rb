84cfff7e2689a48ecbf4ffe402568641168d4adb
Kirby Campbell <jkirbycampbell@gmail.com>
2018-10-22 22:46:19 +0000

    automatically backed up by learn

--- app/workers/songs_worker.rb
+++ app/workers/songs_worker.rb
@@ -4,8 +4,8 @@ class SongsWorker
 
   def perform(songs_file)
     CSV.foreach(songs_file, headers: true) do |song|
-    Song.create(title: song[0])
-    Artist.find_or_create_by(name: song[1])
+      Song.create(title: song[0])
+      Artist.find_or_create_by(name: song[1])
     end
   end
 end
